064d49e460d828c34a751f5b61191434ad7e6df1
Liu <x266liu@cmlabmac12.nexus.uwaterloo.ca>
2017-11-15 18:24:18 -0500

    me

--- Python/__init__.py
+++ Python/__init__.py
@@ -3,8 +3,14 @@ from tkinter import *
 from helper import *
 from functools import partial
 import sys #for sys.exit
+import os
 
 root = tkinter.Tk()
-tcl = root.eval
-tcl("package require loon")
-tcl('namespace import loon::*')  
+tcl = root.tk.call
+root.eval('lappend auto_path /usr/local/Cellar/tcl-tk/8.6.7/lib/loon')
+root.eval('lappend auto_path1 /usr/local/Cellar/tcl-tk/8.6.7/lib/Img')
+icon_fname = os.path.join('/usr/local/Cellar/tcl-tk/8.6.7/lib/loon', 'images', 'LoonIcon.gif')
+img_l = PhotoImage(file = icon_fname)
+root.eval("package require loon")
+root.eval("package require Img")
+root.eval('namespace import loon::*')  
--- Python/l_toplevel.py
+++ Python/l_toplevel.py
@@ -2,19 +2,17 @@ from __init__ import *
 
 
 
-
 def l_toplevel(path = None):      
     if path is None:
         i = 0
         child = ".l" + str(i) 
-        while(stb(child.winfo_exists())):
+        while(bool(tcl("winfo", "exists", child))):
             i = i + 1
             child = '.l' + str(i)   
         path = child
     
-    tt = str(toplevel.title(path))
-    
-    tt.wm_iconphotor(default="::loon::loonIcon")
+    tt = str(tcl("toplevel", path))
+    tcl("wm", "iconphoto", tt,  "-default", img_l)
     return tt
     
 
